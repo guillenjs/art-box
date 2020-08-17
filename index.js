9165f097827a074133d8a4b38c196a9866d7f946
Ginger-Mano <mcasimirian@gmail.com>
2020-08-17 15:03:27 -0400

    begin login

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+let mainContainer = document.querySelector('.container')
+
 
 fetch('http://localhost:3000/artists')
 .then(res => res.json())
 .then(artistArr => console.log(artistArr))
+
+window.addEventListener('DOMContentLoaded', (evt) => {
+    logInForm()
+})
+
+let logInForm = () => {
+    let newForm = document.createElement('form')
+    let newInput = document.createElement('input')
+    let newFormButton = document.createElement('button')
+    newFormButton.innerText = "submit"
+    newForm.append(newInput, newFormButton)
+    mainContainer.append(newForm)
+
+
+}
